dca8e6f2e9be1346030d3cd0508cb7e6e68cc7fe
Hrithik <hc388@njit.edu>
2020-07-18 01:55:47 -0400

    Added Dropdown Support

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navigator from './Navigator'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './newStyles.css';
+import Body from "./weatherBody";
+import GetWeather from "./GetWeather";
+
 
 
 
@@ -25,19 +28,10 @@ const App = () => {
             <h2 id={"num"}>0</h2>
             <button onClick={plusOperator}>+</button>*/}
             {Navigator}
-            <header className="jumbotron" style={{paddingBottom: "0 px"}}>
-                <div className="container" style={{paddingTop: "0 px"}}>
-                    <div className="row row-header">
-                        <div className="col-12 col-sm-6">
-                            <h1>Weather Today</h1>
-                            {/* eslint-disable-next-line react/no-unescaped-entities */}
-                            <p>This segment of the website displays location's weather using a web API from
-                                openweathermap!</p>
-                        </div>
+            {Body}
+            <GetWeather />
 
-                    </div>
-                </div>
-            </header>
+
         </div>
 
 
